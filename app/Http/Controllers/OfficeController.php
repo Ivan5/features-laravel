4f6000ac50ebc0b87a9266dea9b0c35e852f875f
Ivan Jaramillo <ivan@aiwifi.io>
2021-12-29 15:06:10 -0600

    Make endpoint to createa office and make test for that

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -7,10 +7,14 @@ use App\Models\Office;
 use App\Models\Reservation;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Arr;
+use Illuminate\Validation\Rule;
 
 class OfficeController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request): AnonymousResourceCollection
     {
         $offices = Office::query()
             ->with(['images','tags','user'])
@@ -25,11 +29,44 @@ class OfficeController extends Controller
         return OfficeResource::collection($offices);
     }
 
-    public function show(Office $office)
+    public function show(Office $office): OfficeResource
     {
         $office->loadCount(['reservations' => fn ($builder) => $builder->where('status', Reservation::STATUS_ACTIVE)])
             ->load(['images','tags','user']);
             
+        return OfficeResource::make($office);
+    }
+
+    public function create(): JsonResource
+    {
+        if(! auth()->user()->tokenCan('office.create')){
+            abort(403);
+        }
+
+        $data = validator(
+            request()->all(),
+            [
+                'title' => ['string','required'],
+                'description' => ['string','required'],
+                'lat' => ['required', 'numeric'],
+                'lng' => ['required', 'numeric'],
+                'address_line1' => ['required', 'string'],
+                'hidden' => ['bool'],
+                'price_per_day' => ['required','integer','min:100'],
+                'monthly_discount' => ['integer','min:0'],
+
+                'tag' => ['array'],
+                'tags.*' => ['integer', Rule::exists('tags','id')]
+            ]
+        )->validate();
+
+        $data['user_id'] = auth()->id();
+        $data['approval_status'] = Office::APPROVAL_PENDING;
+
+        $office = Office::create(Arr::except($data, ['tags']));
+
+        $office->tags()->sync($data['tags']);
+
         return OfficeResource::make($office);
     }
 }
